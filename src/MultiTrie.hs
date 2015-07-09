34427f9d92e8f968567b7807beead9d088a744ad
Vadim Vinnik <vadim.vinnik@gmail.com>
2015-07-09 20:00:32 +0300

    Implement a few simple functions

--- src/MultiTrie.hs
+++ src/MultiTrie.hs
@@ -1,6 +1,9 @@
 module MultiTrie where
 
+import Prelude hiding (lookup, map)
+import qualified Data.Map as M
 import qualified Data.Map.Lazy as M
+import qualified Data.List as L
 
 data MultiTrie n v = MultiTrie
     {
@@ -8,31 +11,24 @@ data MultiTrie n v = MultiTrie
         children :: M.Map n (MultiTrie n v)
     }
     deriving (Eq, Show)
-    -- todo: redefine (==) and show
-    
+
 empty :: MultiTrie n v
 empty = MultiTrie [] M.empty
 
 singleton :: v -> MultiTrie n v
 singleton x = MultiTrie [x] M.empty
 
-toMap :: MultiTrie n v -> M.Map [n] [v]
-toMap = undefined
-
-fromList :: [([n], v)] -> MultiTrie n v
-fromList = undefined
-
 null :: MultiTrie n v -> Bool
-null = undefined
+null (MultiTrie vs m) = (L.null vs) && (M.null m)
 
 size :: MultiTrie n v -> Int
-size = undefined
+size (MultiTrie vs m) = (L.length vs) + (M.fold undefined 0 m)
 
 lookup :: [n] -> MultiTrie n v -> MultiTrie n v
 lookup = undefined
 
 fetch :: [n] -> MultiTrie n v -> [v]
-fetch = undefined
+fetch ns = values . lookup ns
 
 update :: (MultiTrie n v -> MultiTrie n v) -> [n] -> MultiTrie n v -> MultiTrie n v
 update = undefined
@@ -50,7 +46,7 @@ superpose :: [n] -> MultiTrie n v -> MultiTrie n v -> MultiTrie n v
 superpose = undefined
 
 map :: (v -> w) -> MultiTrie n v -> MultiTrie n w
-map = undefined
+map f (MultiTrie vs m) = MultiTrie (L.map f vs) (M.map (map f) m)
 
 apply :: MultiTrie n (v -> w) -> MultiTrie n v -> MultiTrie n w
 apply = undefined
@@ -60,6 +56,12 @@ union = undefined
 
 bind :: MultiTrie n v -> (v -> MultiTrie n w) -> MultiTrie n w
 bind = undefined
+
+toMap :: MultiTrie n v -> M.Map [n] [v]
+toMap = undefined
+
+fromList :: [([n], v)] -> MultiTrie n v
+fromList = L.foldr (uncurry insert) empty
 
 showTree :: (Show n, Show v) => MultiTrie n v -> String
 showTree = undefined
