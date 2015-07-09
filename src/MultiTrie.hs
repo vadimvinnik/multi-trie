104ad4b288a31c58a7d2016d9084ab1e30b1f865
Vadim Vinnik <vadim.vinnik@gmail.com>
2015-07-09 17:10:56 +0300

    Add two simplest functions and tests thereof

--- src/MultiTrie.hs
+++ src/MultiTrie.hs
@@ -1,2 +1,23 @@
 module MultiTrie where
 
+import qualified Data.Map.Lazy as M
+
+data MultiTrie n v = MultiTrie
+    {
+        values :: [v],
+        children :: M.Map n v
+    }
+
+empty :: MultiTrie n v
+empty = MultiTrie
+    {
+        values = [],
+        children = M.empty
+    }
+
+singleton :: v -> MultiTrie n v
+singleton x = MultiTrie
+    {
+        values = [x],
+        children = M.empty
+    }
--- tests/MultiTrieTest.hs
+++ tests/MultiTrieTest.hs
@@ -1,10 +1,23 @@
 {-# OPTIONS_GHC -F -pgmF htfpp #-}
 module MultiTrieTest where
+
 import MultiTrie
-
-
+import qualified Data.Map.Lazy as M
 import Test.Framework
 
-test_succeeds =  assertBool True
+type TestMultiTrie = MultiTrie String Int
 
-test_fails = assertBoolVerbose "Must fail" False
+test_empty =
+    do
+        assertEqual (values m) []
+        assertEqual (M.toList $ children m) []
+    where
+        m = empty :: TestMultiTrie
+
+test_singleton =
+    do
+        assertEqual (values m) [x]
+        assertEqual (M.toList $ children m) []
+    where
+        m = singleton x :: TestMultiTrie
+        x = 0
