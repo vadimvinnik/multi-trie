3db2f41ad375deb6a2bbdc1e3c040e1f75e09e95
Vadim Vinnik <vadim.vinnik@gmail.com>
2015-08-02 13:50:59 +0300

    Create a top-level reexporting Container module

---
+++ Data/Container.hs
@@ -0,0 +1,9 @@
+module Data.Container(
+    module Data.Container.Base,
+    module Data.Container.SetFromList,
+    module Data.Container.MultisetFromList)
+where
+
+import Data.Container.Base
+import Data.Container.SetFromList
+import Data.Container.MultisetFromList
