6e7df4d3540d85c55fc49b6161ae7fc7ef5be621
Vadim Vinnik <vadim.vinnik@gmail.com>
2015-08-02 14:22:11 +0300

    Add Container instance for Data.Set

--- Data/Container.hs
+++ Data/Container.hs
@@ -1,9 +1,11 @@
 module Data.Container(
     module Data.Container.Base,
     module Data.Container.SetFromList,
-    module Data.Container.MultisetFromList)
+    module Data.Container.MultisetFromList,
+    module Data.Container.Set)
 where
 
 import Data.Container.Base
 import Data.Container.SetFromList
 import Data.Container.MultisetFromList
+import Data.Container.Set
---
+++ Data/Container/Set.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
+
+module Data.Container.Set where
+
+import qualified Data.Set as S
+import Data.Container.Base
+import Data.Allable
+
+instance Ord a => Elementary a S.Set where
+    null = S.null
+    elem = S.member
+    empty = S.empty
+    singleton = S.singleton
+
+instance Ord a => Insertable a S.Set where
+    insert = S.insert
+
+instance Ord a => Deletable a S.Set where
+    delete = S.delete
+
+instance Ord a => Unitable a S.Set where
+    union = S.union
+
+instance Ord a => Intersectible a S.Set where
+    intersection = S.intersection
+
+instance Ord a => Fullable a S.Set where
+    full = S.singleton
+
+instance (Enum a, Bounded a, Ord a) => Topable a S.Set where
+    top = S.fromDistinctAscList allValues
